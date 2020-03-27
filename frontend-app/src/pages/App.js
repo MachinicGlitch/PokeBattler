f5fbf6ea26b51ce7155d23d016f20d1713bfacd9
dbrockm <darinbrockmann@gmail.com>
2020-03-27 14:34:50 -0500

    file restructure and cool background added

---
+++ frontend-app/src/pages/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+//import BattleArea from './../components/BattleArea';
+import './../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Border">
+        <p>
+         <b>Pokemon Battle Simulator</b>
+        </p>
+      </div>
+
+      <div className="Statistics">
+        <p>
+          Testing testing hello 
+        </p>
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
