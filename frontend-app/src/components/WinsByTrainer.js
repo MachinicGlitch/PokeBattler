a2359f5d47dddf663c2d6d5f901687158e6820c7
JHoffstead-Discover <jonathanhoffstead@discover.com>
2020-04-07 16:57:10 -0500

    Implementation of Charts! Co-authored-by: Endri

--- frontend-app/src/components/WinsByTrainer.js
+++ frontend-app/src/components/WinsByTrainer.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import Chart from 'chart.js'
 
-class WinsByType extends Component {
+class WinsByTrainer extends Component {
     chartRef = React.createRef();
     componentDidMount() {
         const myChartRef = this.chartRef.current.getContext("2d");
         
         new Chart(myChartRef, {
-            type: "doughnut",
+            type: "pie",
             data: {
                 //Bring in data
-                labels: ["Normal", "Fighting", "Grass","Poison","Fire","Flying","Water","Bug","Electric","Ground","Psychic","Rock","Ice","Ghost","Dragon"],
+                labels: ["Blue", "Red"],
                 datasets: [
                     {
-                        label: "Wins by Type",
-                        data: [24, 8, 14,33,12,19,32,12,9,14,14,11,5,3,3],
-                        backgroundColor:[
-                            "#A8A77A","#C22E28","#7AC74C","#A33EA1","#EE8130","#A98FF3","#6390F0","#A6B91A","#F7D02C","#E2BF65","#F95587","#B6A136","#96D9D6","#735797","#6F35FC"
+                        label: "Total Wins",
+                        data: [2342, 2189],
+                        backgroundColor: [
+                            "#236B8E",
+                            "#EE6363"
                         ]
                     }
                 ],
             },
             options: {
-                //Customize chart options
+
+
             }
         });
     }
@@ -39,4 +41,4 @@ class WinsByType extends Component {
         )
     }
 }
-export default WinsByType;
+export default WinsByTrainer;
