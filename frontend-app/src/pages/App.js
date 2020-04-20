1feedea33baf8d13fa963f5c77f23285a24fcee9
JHoffstead-Discover <jonathanhoffstead@discover.com>
2020-04-20 12:24:55 -0500

    Banner actually

--- frontend-app/src/pages/App.js
+++ frontend-app/src/pages/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import BattleArea from "./../components/BattleArea";
 import GraphTabs from "./../components/GraphTabs";
+import PokeBanner from "../components/PokeBanner";
 import "./../css/App.css";
 
 function App() {
   return (
     <div className="App">
+      <div className="Banner">
+        <PokeBanner />
+      </div>
       <div className="Border">
         <BattleArea />
       </div>
