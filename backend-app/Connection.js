cd7d56d714493e0b8215a20dc583ad6986dc98a6
MachinicGlitch <51062237+MachinicGlitch@users.noreply.github.com>
2020-12-21 10:40:20 -0600

    Update Connection.js

--- backend-app/Connection.js
+++ backend-app/Connection.js
@@ -1,4 +1,4 @@
-var JAWSDB_URL = 'mysql://d7vm6xybyv5q10ke:qw549vexy5ucerih@zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ya2f30o1en2sad6n'
+var JAWSDB_URL = process.env.JAWSDB_URL
 var mysql = require('mysql');
 
 var connection = mysql.createConnection(JAWSDB_URL);
